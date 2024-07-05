1839786e09682677bb3e7f53cf3daf3c23607d10
f7deat <tan.dct9x@gmail.com>
2024-07-05 14:49:51 +0700

    slide event

--- app/components/event.tsx
+++ app/components/event.tsx
@@ -42,12 +42,12 @@ const EventComponent: React.FC = () => {
             </div>
             <div className="md:w-1/2">
                 <div className="bg-red-600 h-full text-white p-4 md:p-10 2xl:p-20">
-                    <div className="text-2xl md:text-3xl font-bold mb-1" data-aos="fade-up" data-aos-duration="1000">HOẠT ĐỘNG PHONG TRÀO</div>
-                    <div className="text-2xl md:text-3xl font-bold mb-2" data-aos="fade-up" data-aos-duration="1000">PHONG PHÚ</div>
-                    <div className="mb-2">
+                    <div className="text-xl md:text-2xl xl:text-3xl font-bold mb-1" data-aos="fade-up" data-aos-duration="1000">HOẠT ĐỘNG PHONG TRÀO</div>
+                    <div className="text-xl md:text-2xl xl:text-3xl font-bold mb-2" data-aos="fade-up" data-aos-duration="1000">PHONG PHÚ</div>
+                    <div className="mb-2 text-sm md:text-base" data-aos="fade-up" data-aos-duration="1000">
                         Tham gia hoạt động phong trào giúp sinh viên trau dồi kinh nghiệm xã hội, kỹ năng làm việc nhóm và các kỹ năng mềm khác.
                     </div>
-                    <div className="mb-4">
+                    <div className="mb-4 text-sm md:text-base" data-aos="fade-up" data-aos-duration="1000">
                         Và còn muôn vàn các hoạt động sôi nổi, bắt nhịp xu hướng để các tân sinh viên thỏa sức rèn luyện, vui chơi, giải trí; phát triển toàn diện Đức - Trí - Thể - Mỹ, khai phá sức sáng tạo. Chắc chắc quãng đại học của các bạn sẽ không bao giờ nhàm chán và thiếu đi những kỷ niệm đẹp tại mái trường Đại học Hải Phòng.
                     </div>
                     <div className="grid grid-cols-3 gap-4 mb-4">
@@ -61,12 +61,12 @@ const EventComponent: React.FC = () => {
 
                     </div>
                     <div className="flex gap-4 text-white pt-4">
-                        <button type="button" className="border-2 rounded-full h-14 w-14 hover:bg-white hover:text-orange-500" onClick={() => {
+                        <button type="button" className="border-2 rounded-full h-10 md:h-14 w-10 md:w-14 hover:bg-white hover:text-orange-500" onClick={() => {
                             swiperRef.current?.swiper.slidePrev();
                         }}>
                             <FontAwesomeIcon icon={faArrowLeft} />
                         </button>
-                        <button type="button" className="border-2 rounded-full h-14 w-14 hover:bg-white hover:text-orange-500" onClick={() => {
+                        <button type="button" className="border-2 rounded-full h-10 w-10 md:h-14 md:w-14 hover:bg-white hover:text-orange-500" onClick={() => {
                             swiperRef.current?.swiper.slideNext();
                         }}>
                             <FontAwesomeIcon icon={faArrowRight} />
--- app/components/infrastructure.tsx
+++ app/components/infrastructure.tsx
@@ -1,16 +1,19 @@
 "use client"
-import { Fragment, useEffect, useState } from "react";
+import { Fragment, useEffect, useRef, useState } from "react";
 import { Heading1 } from ".";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import Image from "next/image";
+import { Swiper, SwiperRef, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
 
 const Infrastructure: React.FC = () => {
 
     const [galleries, setGalleries] = useState<any[]>([]);
+    const swiperRef = useRef<SwiperRef>(null);
 
     useEffect(() => {
-        fetch('https://dhhp.edu.vn/open-api/galleries?apikey=lJQnpvudu92zeOZ&language=1&type=1&pageIndex=1&pageSize=10&searchTerm=').then(response => response.json().then(data => {
+        fetch('https://dhhp.edu.vn/open-api/galleries?apikey=lJQnpvudu92zeOZ&language=1&type=1&pageIndex=1&pageSize=8&searchTerm=').then(response => response.json().then(data => {
             setGalleries(data);
         }))
     }, []);
@@ -30,70 +33,149 @@ const Infrastructure: React.FC = () => {
                         subTitle="Quần thể nhà trường rộng gần 30ha tọa lạc tại Kiến An - Hải Phòng"
                     />
                     <div className="flex gap-2 md:gap-4">
-                        <button type="button" className="h-10 md:h-12 w-10 md:w-12 border-2 border-red-600 text-red-600 rounded-full hover:bg-red-600 hover:text-white transaction duration-500">
+                        <button
+                            onClick={() => {
+                                swiperRef.current?.swiper.slidePrev();
+                            }}
+                            type="button" className="h-10 md:h-12 w-10 md:w-12 border-2 border-red-600 text-red-600 rounded-full hover:bg-red-600 hover:text-white transaction duration-500">
                             <FontAwesomeIcon icon={faArrowLeft} />
                         </button>
-                        <button type="button" className="h-10 md:h-12  w-10 md:w-12 border-2 border-red-600 text-red-600 rounded-full hover:bg-red-600 hover:text-white transaction duration-500">
+                        <button
+                            onClick={() => {
+                                swiperRef.current?.swiper.slideNext();
+                            }}
+                            type="button" className="h-10 md:h-12  w-10 md:w-12 border-2 border-red-600 text-red-600 rounded-full hover:bg-red-600 hover:text-white transaction duration-500">
                             <FontAwesomeIcon icon={faArrowRight} />
                         </button>
                     </div>
                 </div>
-                <div className="md:flex gap-2 md:gap-4">
-                    <div className="md:w-1/2 md:flex flex-col gap-2 md:gap-4">
-                        <div className="flex md:gap-4 text-sm md:text-base">
-                            <div className="w-1/2">
-                                <div className="w-full h-52 md:h-64 bg-center relative cursor-pointer">
-                                    <Image src={galleries[0].thumbnail} alt="IMG" className="h-full w-full object-cover" width={372} height={256} />
-                                    <div className="h-32 absolute bottom-0 left-0 right-0 p-4 bg-cover bg-center text-white rounded flex items-end" style={{
-                                        backgroundImage: 'url(https://w.ladicdn.com/s600x600/5c7362c6c417ab07e5196b05/49-20240131065024-fdars.png)'
-                                    }}>
-                                        <div>
-                                            <div className="text-lg uppercase font-semibold line-clamp-1">{galleries[0].title}</div>
-                                            <div className="line-clamp-1">{galleries[0].description}</div>
+                {
+                    galleries.length > 0 && (
+                        <Swiper
+                            autoplay
+                            modules={[Autoplay]}
+                            loop
+                            ref={swiperRef}
+                        >
+                            <SwiperSlide>
+                                <div className="md:flex gap-2 md:gap-4">
+                                    <div className="md:w-1/2 md:flex flex-col gap-2 md:gap-4">
+                                        <div className="flex md:gap-4 text-sm md:text-base gap-2">
+                                            <div className="w-1/2 mb-2">
+                                                <div className="w-full h-52 md:h-64 bg-center relative cursor-pointer">
+                                                    <Image src={galleries[0].thumbnail} alt="IMG" className="h-full w-full object-cover" width={372} height={256} />
+                                                    <div className="h-32 absolute bottom-0 left-0 right-0 p-4 bg-cover bg-center text-white rounded flex items-end" style={{
+                                                        backgroundImage: 'url(https://w.ladicdn.com/s600x600/5c7362c6c417ab07e5196b05/49-20240131065024-fdars.png)'
+                                                    }}>
+                                                        <div>
+                                                            <div className="text-lg uppercase font-semibold line-clamp-1">{galleries[0].title}</div>
+                                                            <div className="line-clamp-1">{galleries[0].description}</div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="w-1/2">
+                                                <div className="w-full  h-52 md:h-64 bg-center rounded relative">
+                                                    <Image src={galleries[1].thumbnail} alt="IMG" className="h-full w-full object-cover" width={372} height={256} />
+                                                    <div className="h-32 absolute bottom-0 left-0 right-0 p-4 bg-cover bg-center text-white flex items-end rounded" style={{
+                                                        backgroundImage: 'url(https://w.ladicdn.com/s600x600/5c7362c6c417ab07e5196b05/49-20240131065024-fdars.png)'
+                                                    }}>
+                                                        <div>
+                                                            <div className="text-lg uppercase font-semibold line-clamp-1">{galleries[1].title}</div>
+                                                            <div className="line-clamp-1">{galleries[1].description}</div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="relative h-64">
+                                            <Image src={galleries[2].thumbnail} alt="IMG" className="h-full w-full object-cover" width={760} height={256} />
+                                            <div className="h-32 absolute bottom-0 left-0 right-0 p-4 bg-center text-white flex items-end rounded" style={{
+                                                backgroundImage: 'url(https://w.ladicdn.com/s600x600/5c7362c6c417ab07e5196b05/49-20240131065024-fdars.png)'
+                                            }}>
+                                                <div>
+                                                    <div className="text-lg uppercase font-semibold line-clamp-1">{galleries[2].title}</div>
+                                                    <div className="line-clamp-1">{galleries[2].description}</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="md:w-1/2">
+                                        <div className="relative h-full">
+                                            <Image src={galleries[3].thumbnail} alt="IMG" className="h-full w-full object-cover rounded" width={760} height={528} />
+                                            <div className="h-1/2 text-center bg-no-repeat bg-cover bg-center absolute bottom-0 left-0 right-0 p-4 bg-center text-white flex items-end rounded" style={{
+                                                backgroundImage: 'url(https://w.ladicdn.com/s600x600/5c7362c6c417ab07e5196b05/49-20240131065024-fdars.png)'
+                                            }}>
+                                                <div>
+                                                    <div className="text-lg uppercase font-semibold line-clamp-1">{galleries[3].title}</div>
+                                                    <div className="line-clamp-1">{galleries[3].description}</div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                            <div className="w-1/2">
-                                <div className="w-full  h-52 md:h-64 bg-center rounded relative">
-                                    <img src={galleries[1].thumbnail} alt="IMG" className="h-full w-full object-cover" />
-                                    <div className="h-32 absolute bottom-0 left-0 right-0 p-4 bg-cover bg-center text-white flex items-end rounded" style={{
-                                        backgroundImage: 'url(https://w.ladicdn.com/s600x600/5c7362c6c417ab07e5196b05/49-20240131065024-fdars.png)'
-                                    }}>
-                                        <div>
-                                            <div className="text-lg uppercase font-semibold line-clamp-1">{galleries[1].title}</div>
-                                            <div className="line-clamp-1">{galleries[1].description}</div>
+                            </SwiperSlide>
+                            <SwiperSlide>
+                                <div className="md:flex gap-2 md:gap-4">
+                                    <div className="md:w-1/2 md:flex flex-col gap-2 md:gap-4">
+                                        <div className="flex md:gap-4 text-sm md:text-base gap-2">
+                                            <div className="w-1/2 mb-2">
+                                                <div className="w-full h-52 md:h-64 bg-center relative cursor-pointer">
+                                                    <Image src={galleries[4].thumbnail} alt="IMG" className="h-full w-full object-cover" width={372} height={256} />
+                                                    <div className="h-32 absolute bottom-0 left-0 right-0 p-4 bg-cover bg-center text-white rounded flex items-end" style={{
+                                                        backgroundImage: 'url(https://w.ladicdn.com/s600x600/5c7362c6c417ab07e5196b05/49-20240131065024-fdars.png)'
+                                                    }}>
+                                                        <div>
+                                                            <div className="text-lg uppercase font-semibold line-clamp-1">{galleries[4].title}</div>
+                                                            <div className="line-clamp-1">{galleries[4].description}</div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div className="w-1/2 mb-2">
+                                                <div className="w-full  h-52 md:h-64 bg-center rounded relative">
+                                                    <Image src={galleries[5].thumbnail} alt="IMG" className="h-full w-full object-cover" width={372} height={256} />
+                                                    <div className="h-32 absolute bottom-0 left-0 right-0 p-4 bg-cover bg-center text-white flex items-end rounded" style={{
+                                                        backgroundImage: 'url(https://w.ladicdn.com/s600x600/5c7362c6c417ab07e5196b05/49-20240131065024-fdars.png)'
+                                                    }}>
+                                                        <div>
+                                                            <div className="text-lg uppercase font-semibold line-clamp-1">{galleries[5].title}</div>
+                                                            <div className="line-clamp-1">{galleries[5].description}</div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div className="relative h-64">
+                                            <Image src={galleries[6].thumbnail} alt="IMG" className="h-full w-full object-cover" width={760} height={256} />
+                                            <div className="h-32 absolute bottom-0 left-0 right-0 p-4 bg-center text-white flex items-end rounded" style={{
+                                                backgroundImage: 'url(https://w.ladicdn.com/s600x600/5c7362c6c417ab07e5196b05/49-20240131065024-fdars.png)'
+                                            }}>
+                                                <div>
+                                                    <div className="text-lg uppercase font-semibold line-clamp-1">{galleries[6].title}</div>
+                                                    <div className="line-clamp-1">{galleries[6].description}</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="md:w-1/2">
+                                        <div className="relative h-full">
+                                            <Image src={galleries[7].thumbnail} alt="IMG" className="h-full w-full object-cover rounded" width={760} height={528} />
+                                            <div className="h-1/2 text-center bg-no-repeat bg-cover bg-center absolute bottom-0 left-0 right-0 p-4 bg-center text-white flex items-end rounded" style={{
+                                                backgroundImage: 'url(https://w.ladicdn.com/s600x600/5c7362c6c417ab07e5196b05/49-20240131065024-fdars.png)'
+                                            }}>
+                                                <div>
+                                                    <div className="text-lg uppercase font-semibold line-clamp-1">{galleries[7].title}</div>
+                                                    <div className="line-clamp-1">{galleries[7].description}</div>
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div className="relative h-64">
-                            <img src={galleries[2].thumbnail} alt="IMG" className="h-full w-full object-cover" />
-                            <div className="h-32 absolute bottom-0 left-0 right-0 p-4 bg-center text-white flex items-end rounded" style={{
-                                backgroundImage: 'url(https://w.ladicdn.com/s600x600/5c7362c6c417ab07e5196b05/49-20240131065024-fdars.png)'
-                            }}>
-                                <div>
-                                    <div className="text-lg uppercase font-semibold line-clamp-1">{galleries[2].title}</div>
-                                    <div className="line-clamp-1">{galleries[2].description}</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="md:w-1/2">
-                        <div className="relative h-full">
-                            <img src={galleries[3].thumbnail} alt="IMG" className="h-full w-full object-cover rounded" />
-                            <div className="h-1/2 text-center bg-no-repeat bg-cover bg-center absolute bottom-0 left-0 right-0 p-4 bg-center text-white flex items-end rounded" style={{
-                                backgroundImage: 'url(https://w.ladicdn.com/s600x600/5c7362c6c417ab07e5196b05/49-20240131065024-fdars.png)'
-                            }}>
-                                <div>
-                                    <div className="text-lg uppercase font-semibold line-clamp-1">{galleries[3].title}</div>
-                                    <div className="line-clamp-1">{galleries[3].description}</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                            </SwiperSlide>
+                        </Swiper>
+                    )
+                }
             </div>
         </div>
     )
